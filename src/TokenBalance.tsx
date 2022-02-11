30949973450d346bb2fcf612fcd1f74e1ad5f1dd
SRVng <snakglom@gmail.com>
2022-02-12 02:50:57 +0700

    init

---
+++ src/TokenBalance.tsx
@@ -0,0 +1,34 @@
+import { ethers } from 'ethers'
+import React from 'react'
+import { getContractWithSigner, getProvider, transactionPopup } from './utils'
+
+interface TokenBalanceProps {
+    token: ethers.Contract,
+    signer: string | ethers.providers.JsonRpcSigner
+    setBalance: Function
+}
+
+const TokenBalance = (props: TokenBalanceProps) => {
+
+    const contractWithSigner = getContractWithSigner(props.token, props.signer);
+    const address = getProvider().getSigner().getAddress();
+    
+    const fetchBalance = async () => {
+        let tx = await contractWithSigner.balanceOf(address);
+        let balance = ethers.utils.formatEther(tx);
+        props.setBalance(balance);
+    }
+
+    React.useEffect(() => {
+
+        setInterval(() => {
+            fetchBalance();
+        }, 30000);
+
+        fetchBalance();
+    }, [])
+
+    return (<></>);
+}
+
+export default TokenBalance
